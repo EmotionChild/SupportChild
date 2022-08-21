0e5a93a6a68205cb8fb1016481d4f78e74a428d8
EmotionChild <JDRCaple@outlook.com>
2022-08-21 19:33:27 +1200

    Updated general files

--- SupportChild/Transcriber.cs
+++ SupportChild/Transcriber.cs
@@ -3,60 +3,51 @@ using System.Threading.Tasks;
 
 using DiscordChatExporter.Core.Discord;
 using DiscordChatExporter.Core.Discord.Data;
-using DiscordChatExporter.Core.Exceptions;
 using DiscordChatExporter.Core.Exporting;
 using DiscordChatExporter.Core.Exporting.Filtering;
 using DiscordChatExporter.Core.Exporting.Partitioning;
-using DiscordChatExporter.Core.Utils.Extensions;
 
-namespace SupportChild
+namespace SupportChild;
+
+internal static class Transcriber
 {
-    internal static class Transcriber
-    {
-        internal static async Task ExecuteAsync(ulong channelID, uint ticketID)
-        {
-            DiscordClient discordClient = new DiscordClient(new AuthToken(AuthTokenKind.Bot, Config.token));
-            ChannelExporter Exporter = new ChannelExporter(discordClient);
+	internal static async Task ExecuteAsync(ulong channelID, uint ticketID)
+	{
+		DiscordClient discordClient = new DiscordClient(Config.token);
+		ChannelExporter exporter = new ChannelExporter(discordClient);
 
-            if (!Directory.Exists("./transcripts"))
-            {
-                Directory.CreateDirectory("./transcripts");
-            }
+		if (!Directory.Exists("./transcripts"))
+		{
+			Directory.CreateDirectory("./transcripts");
+		}
 
-            string dateFormat = "yyyy-MMM-dd HH:mm";
+		Channel channel = await discordClient.GetChannelAsync(new Snowflake(channelID));
+		Guild guild = await discordClient.GetGuildAsync(channel.GuildId);
 
-            // Configure settings
-            if (Config.timestampFormat != "")
-                dateFormat = Config.timestampFormat;
+		ExportRequest request = new ExportRequest(
+			Guild: guild,
+			Channel: channel,
+			OutputPath: GetPath(ticketID),
+			Format: ExportFormat.HtmlDark,
+			After: null,
+			Before: null,
+			PartitionLimit: PartitionLimit.Null,
+			MessageFilter: MessageFilter.Null,
+			ShouldDownloadMedia: false,
+			ShouldReuseMedia: false,
+			DateFormat: "yyyy-MMM-dd HH:mm"
+			);
 
-            Channel channel = await discordClient.GetChannelAsync(new Snowflake(channelID));
-            Guild guild = await discordClient.GetGuildAsync(channel.GuildId);
+		await exporter.ExportChannelAsync(request);
+	}
 
-            ExportRequest request = new ExportRequest(
-                guild: guild,
-                channel: channel,
-                outputPath: GetPath(ticketID),
-                format: ExportFormat.HtmlDark,
-                after: null,
-                before: null,
-                partitionLimit: PartitionLimit.Null,
-                messageFilter: MessageFilter.Null,
-                shouldDownloadMedia: false,
-                shouldReuseMedia: false,
-                dateFormat: dateFormat
-            );
+	internal static string GetPath(uint ticketNumber)
+	{
+		return "./transcripts/" + GetFilename(ticketNumber);
+	}
 
-            await Exporter.ExportChannelAsync(request);
-        }
-
-        internal static string GetPath(uint ticketNumber)
-        {
-            return "./transcripts/" + GetFilename(ticketNumber);
-        }
-
-        internal static string GetFilename(uint ticketNumber)
-        {
-            return "ticket-" + ticketNumber.ToString("00000") + ".html";
-        }
-    }
+	internal static string GetFilename(uint ticketNumber)
+	{
+		return "ticket-" + ticketNumber.ToString("00000") + ".html";
+	}
 }
--- SupportChild/Utilities.cs
+++ SupportChild/Utilities.cs
@@ -1,71 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Security.Cryptography;
+using System.Threading.Tasks;
 using DSharpPlus.Entities;
 
-namespace SupportChild
+namespace SupportChild;
+
+public static class Utilities
 {
-    public static class Utilities
-    {
-        public static List<T> RandomizeList<T>(List<T> list)
-        {
-            RNGCryptoServiceProvider provider = new RNGCryptoServiceProvider();
-            int n = list.Count;
-            while (n > 1)
-            {
-                byte[] box = new byte[1];
-                do provider.GetBytes(box);
-                while (!(box[0] < n * (Byte.MaxValue / n)));
-                int k = (box[0] % n);
-                n--;
-                T value = list[k];
-                list[k] = list[n];
-                list[n] = value;
-            }
+	private static readonly Random rng = new Random();
 
-            return list;
-        }
+	public static void Shuffle<T>(this IList<T> list)
+	{
+		int n = list.Count;
+		while (n > 1)
+		{
+			n--;
+			int k = rng.Next(n + 1);
+			(list[k], list[n]) = (list[n], list[k]);
+		}
+	}
 
-        public static string[] ParseIDs(string args)
-        {
-            if (string.IsNullOrEmpty(args))
-            {
-                return new string[0];
-            }
-            return  args.Trim().Replace("<@!", "").Replace("<@", "").Replace(">", "").Split();
-        }
+	public static LinkedList<string> ParseListIntoMessages(List<string> listItems)
+	{
+		LinkedList<string> messages = new LinkedList<string>();
 
-        public static LinkedList<string> ParseListIntoMessages(List<string> listItems)
-        { 
-            LinkedList<string> messages = new LinkedList<string>();
+		foreach (string listItem in listItems)
+		{
+			if (messages.Last?.Value?.Length + listItem?.Length < 2048)
+			{
+				messages.Last.Value += listItem;
+			}
+			else
+			{
+				messages.AddLast(listItem);
+			}
+		}
 
-            foreach (string listItem in listItems)
-            {
-                if (messages.Last?.Value?.Length + listItem?.Length < 2048)
-                {
-                    messages.Last.Value += listItem;
-                }
-                else
-                {
-                    messages.AddLast(listItem);
-                }
-            }
+		return messages;
+	}
 
-            return messages;
-        }
+	public static async Task<List<Database.Category>> GetVerifiedChannels()
+	{
+		List<Database.Category> verifiedCategories = new List<Database.Category>();
+		foreach (Database.Category category in Database.GetAllCategories())
+		{
+			DiscordChannel channel = null;
+			try
+			{
+				channel = await SupportChild.discordClient.GetChannelAsync(category.id);
+			}
+			catch (Exception) { /*ignored*/ }
 
-        public static DiscordRole GetRoleByName(DiscordGuild guild, string Name)
-        {
-            Name = Name.Trim().ToLower();
-            foreach (DiscordRole role in guild.Roles.Values)
-            {
-                if (role.Name.ToLower().StartsWith(Name))
-                {
-                    return role;
-                }
-            }
-
-            return null;
-        }
-    }
+			if (channel != null)
+			{
+				verifiedCategories.Add(category);
+			}
+			else
+			{
+				Logger.Warn("Category '" + category.name + "' (" + category.id + ") no longer exists! Ignoring...");
+			}
+		}
+		return verifiedCategories;
+	}
 }
